eb41db12c6011c4133519e1604ca7c988ffeb1d0
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-11-30 20:41:54 -0500

    initial version, read only

---
+++ leetcode_template.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Tim H 2023
+# https://leetcode.com/problems/
+# Runtime:
+# Memory:
+""" module docstring """
+
+class Solution:
+    """doc string"""
+
+    def testname(self, paramname: int) -> int:
+        """doc string"""
+
+
+###############################################################################
+#       MAIN
+###############################################################################
+sol = Solution()
+
+tests = [1, 2, 3]
+
+for test_iter in tests:
+    print(str(test_iter) + ' -->  ' + str(sol.testname(test_iter)))
