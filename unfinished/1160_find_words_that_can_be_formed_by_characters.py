2719461204751b647421e5f63e0a97a9a5feb900
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-12-02 19:28:09 -0500

    got it working with custom python module, but leetcode will not install that module, have to rewrite with Counter instead

--- unfinished/1160_find_words_that_can_be_formed_by_characters.py
+++ unfinished/1160_find_words_that_can_be_formed_by_characters.py
@@ -1,59 +1,72 @@
 #!/usr/bin/python3
 # Tim H 2023
 # UNFINISHED
+# Need to use the Counter class instead of multiset
+# Leetcode doesn't allow you to install additional python modules beyond
+# what they have
+# ./.venv/bin/python -m pip install -r requirements.txt
 # https://leetcode.com/problems/two-sum/description/
+# https://multiset.readthedocs.io/en/stable/
+# https://docs.python.org/3.10/library/collections.html#collections.Counter
 """docstring"""
 
-# from collections import Counter
-
-# def sortString(string_to_sort):
-#     from functools import reduce
-#     return reduce(lambda a, b : a + b, sorted(string_to_sort))
+from multiset import Multiset
      
-def sortString2(string_to_sort):
-    return ''.join(sorted(string_to_sort))
+# def sortString2(string_to_sort):
+#     return ''.join(sorted(string_to_sort))
 
-# def find_intersection_excluding_duplicates(lstr,rstr):
-#     return set(lstr).intersection(rstr)
+# Leetcode doesn't allow system calls:
+# import subprocess
+# import sys
+# def install(package):
+#     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-#def is_subset_of(lstr,rstr):
-#    return set(lstr).issubset(set(rstr))
+# import pip
+# def install(package):
+#     if hasattr(pip, 'main'):
+#         pip.main(['install', package])
+#     else:
+#         pip._internal.main(['install', package])
+# install('multiset')
+# from multiset import Multiset
 
+
+def multiset_is_subset_of(l_str, r_str):
+    return Multiset(l_str).issubset(Multiset(r_str))
+    
 class Solution:
     def countCharacters(self, words: list[str], chars: str) -> int:
         goodsum = 0
         l_chars = len(chars)
         for i, iter_word in enumerate(words):
+            # iter_word = sortString2(iter_word)
             # if there are not enough characters to form the word, don't bother
             if len(iter_word) > l_chars:
-                # print('word requires more characters than chars has, impossible')
                 continue
-            # may need to start subtracting letters?
-            if set(iter_word).issubset(set(chars)):                
-                print(sortString2(iter_word) + ' is subset of ' + sortString2(chars))
-                goodsum += len(iter_word)                
+            if Multiset(iter_word).issubset(Multiset(chars)):
+                goodsum += len(iter_word)
         return goodsum
 
 ###############################################################################
 #       MAIN
 ###############################################################################
 
-# test_words = ["cat", "bt", "hat", "tree", "cathyz"]
-# test_chars = "atach"
-# correct_ans = 6
-# ans = Solution().countCharacters(test_words, test_chars)
-# print('INPUT words: ' + str(test_words) + '   chars: ' + test_chars)
-# print('OUTPUT: ' + str(ans) + ' Correct: ' + str(correct_ans))
-# assert ans == correct_ans
+test_words = ["cat", "bt", "hat", "tree", "cathyz"]
+test_chars = "atach"
+correct_ans = 6
+ans = Solution().countCharacters(test_words, test_chars)
+print('INPUT words: ' + str(test_words) + '   chars: ' + test_chars)
+print('OUTPUT: ' + str(ans) + ' Correct: ' + str(correct_ans))
+assert ans == correct_ans
 
 
-# test_words = ["hello","world","leetcode","impossiblylongword"]
-# test_chars = "welldonehoneyr"
-# correct_ans = 10
-# ans = Solution().countCharacters(test_words, test_chars)
-# print('INPUT words: ' + str(test_words) + '   chars: ' + test_chars)
-# print('OUTPUT: ' + str(ans) + ' Correct: ' + str(correct_ans))
-# assert ans == correct_ans
+test_words = ["hello","world","leetcode","impossiblylongword"]
+test_chars = "welldonehoneyr"
+correct_ans = 10
+ans = Solution().countCharacters(test_words, test_chars)
+print('INPUT words: ' + str(test_words) + '   chars: ' + test_chars)
+print('OUTPUT: ' + str(ans) + ' Correct: ' + str(correct_ans))
+assert ans == correct_ans
 
 
 test_words = ["dyiclysmffuhibgfvapygkorkqllqlvokosagyelotobicwcmebnpznjbirzrzsrtzjxhsfpiwyfhzyonmuabtlwin",
@@ -82,4 +95,4 @@ correct_ans = 0
 ans = Solution().countCharacters(test_words, test_chars)
 # print('INPUT words: ' + str(test_words) + '   chars: ' + test_chars)
 # print('OUTPUT: ' + str(ans) + ' Correct: ' + str(correct_ans))
-# assert ans == correct_ans
+assert ans == correct_ans
