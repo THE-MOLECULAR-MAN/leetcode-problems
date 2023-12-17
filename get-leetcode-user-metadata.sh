4e70a79a7ddea6ccc6ec99621ae62dda504144ec
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-12-17 09:33:11 -0500

    initial version

---
+++ get-leetcode-user-metadata.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Tim H 2023
+# https://github.com/JeremyTsaii/leetcode-stats-api
+# https://github.com/akarsh1995/leetcode-graphql-queries
+
+curl https://leetcode-stats-api.herokuapp.com/THE-MOLECULAR-MAN/ | jq .
