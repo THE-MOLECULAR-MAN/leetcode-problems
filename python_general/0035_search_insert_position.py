e68b89e1b7ee4965bbd3f5a72d5f20492903cbb2
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-12-17 23:47:44 -0500

    initial version

---
+++ python_general/0035_search_insert_position.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# Tim H 2023
+# https://leetcode.com/problems/search-insert-position/
+# Runtime:  beats 58%
+# Memory:   beats 39%
+"""docstring"""
+
+import bisect  
+
+class Solution:
+    """docstring"""
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        """docstring"""
+        return bisect.bisect_left(nums, target)
---
+++ unfinished/0088_merge_sorted_array.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# Tim H 2023
+# https://leetcode.com/problems/merge-sorted-array/
+# Runtime:  beats %
+# Memory:   beats %
+"""docstring"""
+
+class Solution:
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if len(nums1) == 0: 
+            nums1 = nums2
+
+        for i1, v1 in enumerate(nums1):            
+            if len(nums2) == 0:
+                return None
+            
+            i2 = 0
+            while (i2 < len(nums2)):
+                if nums2[i2] >= v1:
+                    nums1.insert(nums2[i2],i1)
+                i2 += 1
+            # del nums2[0:i2]
+            nums2 = nums2[i2:]
+            
+
+# nums1 = [1, 2, 3, 0, 0, 0]
+# nums2 = [2, 5, 6]
+# nums1L = len(nums1)
+# nums2L = len(nums2)
+
+
+# Solution().merge(nums1, nums1L, nums2, nums2L)
+# print(nums1)
+
+            
+
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+nums1L = len(nums1)
+nums2L = len(nums2)
+
+
+Solution().merge(nums1, nums1L, nums2, nums2L)
+print(nums1)
