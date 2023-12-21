b65d890e3aac72c54a358fea1e70b9a15dcd2d47
THE-MOLECULAR-MAN <99990915+THE-MOLECULAR-MAN@users.noreply.github.com>
2023-12-21 11:16:08 -0500

    additional commenting for clarity

--- python_general/0125_valid_palindrome.py
+++ python_general/0125_valid_palindrome.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 # Tim H 2023
 # https://leetcode.com/problems/valid-palindrome/
-# Runtime: beats	97%
-# Memory:  beats	13%
+# high speed but high memory utilization approach
+# Runtime: beats	97%     O(n)
+# Memory:  beats	13%     O(n)
+
+# Could be rewritten as iterative approach that doesn't use any extra memory
+# but it'd prob be slower than comparing two strings
 """docstring"""
 
 from re import sub
@@ -14,10 +18,13 @@ class Solution:
     def simplify_string(self, s: str) -> str:
         """removes all non-alphanumeric characters from string and converts
         everything to lower case"""
+        # I think using a compiled regex is a little faster than this?
         return sub("[^0-9a-zA-Z]+", "", s).lower()
 
     def is_even(self, num: int) -> bool:
         """returns boolean if num is an even number """
+        # adding bool() for extra readability for newbies, but it is
+        # not required
         return bool(num % 2 == 0)
 
     def get_mirrors(self, s: str) -> (str, str):
@@ -25,12 +32,20 @@ class Solution:
         is a palindrome."""
         L = len(s)
         if self.is_even(L):
+            # the string has an even number of characters, so we don't need
+            # to worry about ignoring a middle character
             end_of_left = (L - 1) // 2
             start_of_right = end_of_left + 1
         else:
+            # string has an odd number of characters
+            # the first string will go from 0 to 1 character before the middle
+            # the second string will start just after the middle character
             end_of_left = ((L - 1) // 2) - 1
             start_of_right = end_of_left + 2
 
+        # returns the first string forward, and the second string in reverse
+        # using reverse indexes is a little faster than using functions like
+        # reverse()
         return s[0:end_of_left+1], s[L-1:start_of_right-1:-1]
 
     def isPalindrome(self, s: str) -> bool:
@@ -44,15 +59,18 @@ class Solution:
         # strip out the non-alphanumeric characters
         s = self.simplify_string(s)
 
-        # another early exit
-        # split this out b/c some test cases might be a long list of
+        # Early exit for special cases:
+        # Intentionally doing this test AFTER the simplify_string
+        # b/c some test cases might be a long list of
         # non-alphanumerics with only 1 or fewer valid chars to test.
-        # That would be slow
         if len(s) <= 1:
             return True
 
         # fetch the two strings to test if they match
         # uses extra memory, but is pretty fast
         leftside, rightside = self.get_mirrors(s)
+
+        # leftover from debugging:
         # print(s + " " + leftside + " " + rightside)
+
         return leftside == rightside
